4b6bcfffdbe12a6bf14763b16aaead65abdd40a3
juanituza <juanituza85@gmail.com>
2024-05-29 11:14:23 -0300

    commit

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const archivBD = mongo;
 
 // Middleware para permitir CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://crud-mern-ztmt.onrender.com:5173"
+  );
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
