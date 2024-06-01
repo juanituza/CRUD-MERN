8bd742162a9d6c6d69a95e8fae7e12d35096c847
juanituza <juanituza85@gmail.com>
2024-06-01 10:35:54 -0300

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,48 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import "./App.css";
 import AgregarUsuario from "./AgregarUsuarios";
 import EditarUsuario from "./EditarUsuario";
 import ListaUsuarios from "./ListaUsuarios";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
-          <Link className="navbar-brand" to="/">
-            CRUD MERN STACK
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
-                  Inicio
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/agregarusuario">
-                  Agregar Usuario
-                </Link>
-              </li>
-            </ul>
+    <BrowserRouter>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container">
+            <a className="navbar-brand" href="/">
+              CRUD MERN STACK
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <Link className="nav-link active" aria-current="page" to="/">
+                    Inicio
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/agregarusuario">
+                    Agregar Usuario
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-      <BrowserRouter>
+        </nav>
         <Routes>
           <Route path="/" element={<ListaUsuarios />} exact></Route>
           <Route
@@ -52,8 +56,8 @@ function App() {
             exact
           ></Route>
         </Routes>
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    port: 3000,
-  },
-  preview: {
-    port: 5000,
-  },
 });
